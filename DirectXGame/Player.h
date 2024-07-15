1225bdb5f1857daf003c0f6fbdf9d9a675e8e2a8
GC1AChowWingTung <k023g1040@g.neec.ac.jp>
2024-07-15 13:44:48 +0800

    KamataEngineを入れた

---
+++ DirectXGame/Player.h
@@ -0,0 +1,34 @@
+#pragma once
+#pragma once
+
+#include "Model.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include "cassert"
+
+class Player {
+public:
+	Player();
+	~Player();
+
+	void Initialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection);
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	/// texture
+	uint32_t textureHandle_ = 0u;
+	/// 3D
+	Model* model_ = nullptr;
+	/// ViewProjection
+	WorldTransform worldTransform_;
+	ViewProjection* viewProjection_ = nullptr;
+};
